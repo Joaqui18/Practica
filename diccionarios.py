eeeff92df736ebd9c5544260d762b2b870d25f45
joaqui18 <joaquin.viscubi@sagradoalcal.edu.ar>
2023-11-14 15:01:34 -0300

    diccinario

---
+++ diccionarios.py
@@ -0,0 +1,22 @@
+
+
+
+#diccionario = {"nombre": "juanito","apellido": "domingues", "segundo apellido": "sanchez" }
+#print(diccionario)
+
+#diccionario = {"nombre": "juanito","apellido": "domingues", "edad":245}
+#diccionario2= {"nombre": "pedro","apellido": "sanchez", "edad":24}
+#diccionario_union = diccionario, diccionario2
+#print(diccionario_union)
+
+#lista = ["carlos", "anna", "dylan", "pedro", "balsa botin", "pablo", "uva", "caramelo", "cinto", "cemento", "cerrrojo"]
+#diccinario = {}
+#for palabra in lista:
+ #if palabra in diccinario:
+  #   diccinario [palabra] += 1
+#else: diccinario[palabra] = 1
+#print(diccinario)
+
+#diccionario = {}
+#for suma in diccionario:
+     
---
+++ listas.py
@@ -0,0 +1,24 @@
+ #lista = [1,2,2,3,4]
+#lista.extend([5,6,7,8,6,7,8,98,9,9])
+#print(lista)
+
+#lista = [1,2,5,3,4,7,6]
+#lista.extend([5,6,7,8,6,7,8,98,9,9])
+#lista=sorted(lista)
+#print(lista)
+
+#lista = ["hola","adios","espera","veni"]
+#lista=sorted(lista)
+#print(lista)
+
+#lista =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+#suma=0
+#for elemento in lista:
+#    suma += elemento
+#    print("el resultado de la suma es:",suma)
+
+#lista = ["escalera","piso","pantalon","suma","puerta"]
+#for palabra in lista:
+#    longitud = len(palabra)
+#print(f"la palabra '{palabra}' tiene {longitud} caracteres")
+
